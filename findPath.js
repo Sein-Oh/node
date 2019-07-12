58c5b308175e77122e2a3b0ea0887a3d36065f99
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2019-07-12 16:44:06 +0900

    Update findPath.js

--- findPath.js
+++ findPath.js
@@ -1,23 +1,21 @@
-var path = require('path'), fs=require('fs');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = function fromDir(startPath,filter){
+function makeDataset(dataPath, filter){
+    const label = fs.readdirSync(dataPath, {withFileTypes:true}).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
+    console.log('found %s label.', label.length);
+    const fileArray = [];
+    label.forEach(e=> {
+        console.log('-', e);
+        const imgPath = path.join(dataPath, e);
+        const imgName = fs.readdirSync(imgPath).filter((file) => file.endsWith(filter));
+        imgName.forEach(name =>{
+            fileArray.push([e, name]);
+        })
+    });
+    console.log(fileArray);
+}
 
-    //console.log('Starting from dir '+startPath+'/');
-
-    if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
-        return;
-    }
-
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=path.join(startPath,files[i]);
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory()){
-            fromDir(filename,filter); //recurse
-        }
-        else if (filename.indexOf(filter)>=0) {
-            console.log('-- found: ',filename);
-        };
-    };
-};
+const train = './dataset/train'
+const filter = '.png';
+makeDataset(train, filter);
